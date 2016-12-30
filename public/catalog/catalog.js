a17367cb41547655d16ec105292047cbc1a827b0
Glendale Acosta <gglendale17@gmail.com>
2016-12-30 14:37:27 -0800

    Using themoviedb.org api to render movies onSearch in catalog page.

--- public/catalog/catalog.js
+++ public/catalog/catalog.js
@@ -22,34 +22,48 @@ angular.module('myApp.catalog', ['ui.router'])
 
 }])
 
-.controller('catalogController', ['$scope', '$http', function($scope, $http){
+.controller('catalogController', ['$scope', '$http', '$timeout', function($scope, $http, $timeout){
     
-    $scope.movies = [
-        {
-            title: "Men in black",
-            rating: 5
-        },
-        {
-            title: "Moana",
-            rating: 8
-        },
-        {
-            title: "Stranger Things",
-            rating: 11
-        },
-        {
-            title: "Zootopia",
-            rating: 8.3
-        },
-        {
-            title: "Groundhog Day",
-            rating: 9
-        }
-    ]
+
+    $scope.onSearch = function(title){
+        
+        var api_key = '1cc7edd7a3b1549a1de32ac8a417a5e4';
+
+        $http({
+            method: 'GET',
+            url: 'https://api.themoviedb.org/3/search/movie?api_key=' + api_key + '&language=en-US&query=' + title + '&page=1&include_adult=false' 
+        })
+            .then(function(res){
+                // console.log(res.data.results);
+                $scope.movies = res.data.results;
+
+                
+                $timeout(function(){
+                    
+                    var images_arr = document.getElementsByTagName('img');
+                    var images_length = images_arr.length;
+
+                    for(var i = 0; i < images_length; i++) {
+
+                        var src = images_arr[i].src;
+                        var isNull = src.endsWith('null'); 
+                        if (isNull) {
+                            // src = '../images/default_img.jpg';
+                            
+                            images_arr[i].src = '../images/default_img.jpg';
+                        }
+                    }
+
+                }, 500);
+                
+                
+            },function(err){
+                console.log(err);
+            })
+    }
 
 
-
-
+    
     // imdb api key: edb9cf13-36f4-47e8-a724-e6bcdd1148d5
     // http://imdb.wemakesites.net/#anhcor-search-imdb
-}]);
+}])
