183ae7ef14cbd5d28f5562230dbd3207775e3945
Glendale Acosta <gglendale17@gmail.com>
2016-12-31 10:30:49 -0800

    Created slideshow and added basic layouts to homepage.

--- public/catalog/catalog.js
+++ public/catalog/catalog.js
@@ -29,6 +29,7 @@ angular.module('myApp.catalog', ['ui.router'])
         
         var api_key = '1cc7edd7a3b1549a1de32ac8a417a5e4';
 
+        
         $http({
             method: 'GET',
             url: 'https://api.themoviedb.org/3/search/movie?api_key=' + api_key + '&language=en-US&query=' + title + '&page=1&include_adult=false' 
@@ -36,7 +37,6 @@ angular.module('myApp.catalog', ['ui.router'])
             .then(function(res){
                 // console.log(res.data.results);
                 $scope.movies = res.data.results;
-
                 
                 $timeout(function(){
                     
@@ -62,37 +62,6 @@ angular.module('myApp.catalog', ['ui.router'])
     }
 
     
-
-    $scope.equalHeight = function(){
-        
-        var images_arr = document.getElementsByTagName('img');
-        var images_length = images_arr.length;
-        console.log(images_arr);
-        var smallestHeight = 9999;
-
-        for(var i = 0; i < images_length; i++) {
-            // console.log("images_arr[" + i + "] height: " + images_arr[i].height);
-
-            if (images_arr[i].height < smallestHeight) {
-            
-            smallestHeight = images_arr[i].height;
-            
-            }
-        }
-
-        for(var i = 0; i < images_length; i++) {
-            // console.log("images_arr[" + i + "] height: " + images_arr[i].height);
-            // images_arr[i].height = smallestHeight;
-            console.log(smallestHeight);
-            // console.log(images_arr[i].height);
-            images_arr[i].style.maxHeight = smallestHeight + "px";
-            images_arr[i].style.width = "100%";
-        }
-        
-        // Make every img height the same
-
-    }
-
 
     
     // imdb api key: edb9cf13-36f4-47e8-a724-e6bcdd1148d5
--- public/home/home.js
+++ public/home/home.js
@@ -1,4 +1,4 @@
-angular.module('myApp.home', ['ui.router'])
+angular.module('myApp.home', ['ui.router', 'ngAnimate'])
 
 .config(['$stateProvider', function($stateProvider){
 
@@ -19,8 +19,54 @@ angular.module('myApp.home', ['ui.router'])
 
 }])
 
-.controller('homeController', ['$scope', function($scope){
-    
+.controller('homeController', ['$scope', '$interval' , '$timeout', function($scope, $interval, $timeout){
 
+    $scope.imgSrc1 = '../images/main_bg.jpg';
+    $scope.imgSrc2 = '../images/rogue_one_bg.jpg';
+    $scope.imgSrc3 = '../images/moana_bg1.jpg';
+
+    var slide = 1; 
+    $scope.zIndex1 = 5;
+    $scope.zIndex2 = 5;
+    $scope.zIndex3 = 0;
+    // Super basic Slideshow
+    $interval(function(){
+        if (slide === 1) {
+
+        $scope.opac1 = 0;
+        $scope.opac2 = 1;
+        $scope.opac3 = 1;
+
+        $scope.zIndex1 = 5;
+        $scope.zIndex2 = 5;
+        $scope.zIndex3 = 0;
+
+        slide = 2;
+        
+        } else if (slide === 2) {
+            $scope.opac1 = 1;
+            $scope.opac2 = 0;
+            $scope.opac3 = 1;
+
+            $scope.zIndex1 = 0;
+            $scope.zIndex2 = 5;
+            $scope.zIndex3 = 5;
+
+            slide = 3;
+
+        } else if (slide === 3) {
+            $scope.opac1 = 1;
+            $scope.opac2 = 1;
+            $scope.opac3 = 0;
+
+            $scope.zIndex1 = 0;
+            $scope.zIndex2 = 0;
+            $scope.zIndex3 = 5;
+
+            slide = 1;
+
+        }
+
+    }, 6000)
     
 }]);
