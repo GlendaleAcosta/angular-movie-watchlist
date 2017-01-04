12bbbf9e69afe8abd46b9d60fa715b8ed9205e2d
Glendale Acosta <gglendale17@gmail.com>
2017-01-04 11:56:33 -0800

    Fixed angular routing to refresh to correct page.

--- controllers/home.js
+++ controllers/home.js
@@ -1,4 +1,5 @@
 
-module.exports = function(req, res) {
-    res.sendFile('/Users/glendaleacosta/webdev/small_projects/Angular-Apps/angular-movie-watchlist/public/index.html');
+exports.index = function(req, res) {
+    
+    res.sendFile('index.html');
 }
--- public/app.js
+++ public/app.js
@@ -11,10 +11,23 @@ angular.module("myApp", [
 ])
 
 .config(['$stateProvider', '$urlRouterProvider','$locationProvider', function($stateProvider, $urlRouterProvider, $locationProvider){
-    
+        
         $urlRouterProvider.otherwise('/');
 
+        // Allows for urls such as 'example.com/about' & 'example.com/about/'
+        $urlRouterProvider.rule(function($injector, $location) {
 
+            var path = $location.path();
+            var hasTrailingSlash = path[path.length-1] === '/';
+
+            if(hasTrailingSlash) {
+
+            //if last charcter is a slash, return the same url without the slash  
+            var newPath = path.substr(0, path.length - 1); 
+            return newPath; 
+            } 
+
+        });
         
         $locationProvider.html5Mode(true);
 }])
