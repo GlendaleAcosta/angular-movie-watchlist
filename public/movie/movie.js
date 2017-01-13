eac612abb14761025b5d19d18400fc91e1ff0dda
Glendale Acosta <gglendale17@gmail.com>
2017-01-12 22:21:04 -0800

    Added resolve to only allow access to valid profile pages.

--- public/movie/movie.js
+++ public/movie/movie.js
@@ -14,6 +14,7 @@ angular.module('myApp.movie', ['ui.router'])
                     templateUrl: 'movie/movie.html',
                     controller: 'movieController'
                 }
+            
             }
         })
 
--- public/profile/profile.js
+++ public/profile/profile.js
@@ -4,7 +4,7 @@ angular.module('myApp.profile', ['ui.router'])
 
    $stateProvider
         .state('profile', {
-            url: '/profile/:userId',
+            url: '/profile/:userId/:profilePage',
             views: {
                 'navbar': {
                     templateUrl: 'navbar/navbar.html',
@@ -12,16 +12,40 @@ angular.module('myApp.profile', ['ui.router'])
                 },
                 'page': {
                     templateUrl: 'profile/profile.html',
-                    controller: 'profileController'
+                    controller: 'profileController',
+                    resolve: {
+                        validateProfilePage: function($stateParams, $state){
+                            var profilePages = [
+                                'movie-watchlist',
+                                'favorite-movies',
+                                'movies-watched']
+                            var profilePages_length = profilePages.length;
+                            var isValidPage = false;
+
+                            for(var i = 0; i < profilePages_length; i++) {
+                               if ($stateParams.profilePage === profilePages[i]) {
+                                
+                                   isValidPage = true;
+                                   break;
+                               } 
+                            }
+                            
+                            if (isValidPage === false) {
+                                $state.go('home');
+                            }
+                        }
+                    }
                 }
             }
         })
 }])
 
-.controller('profileController', ['$scope', '$http', 'auth' , function($scope, $http, auth){
+.controller('profileController', ['$scope', '$http', '$stateParams', '$state' , 'auth' , function($scope, $http, $stateParams, $state ,auth){
+    
+    
+    var api_key = '1cc7edd7a3b1549a1de32ac8a417a5e4';
     
 
-    var api_key = '1cc7edd7a3b1549a1de32ac8a417a5e4';
     var getWatchlist = function(){
 
         $http({
@@ -32,7 +56,6 @@ angular.module('myApp.profile', ['ui.router'])
             url: '/watchlist'
         })
         .then(function(res){
-            console.log(res.data.watchlist);
             var watchlist_arr = res.data.watchlist;
             var watchlist_length = watchlist_arr.length;
             
@@ -68,7 +91,11 @@ angular.module('myApp.profile', ['ui.router'])
         })
     }
 
-    getWatchlist();
-
+    
+    if ($stateParams.profilePage === 'movie-watchlist') {
+        getWatchlist();
+    } else if ($stateParams.profilePage === 'favorite-movies') {
+        console.log("get Favorite Movies");
+    }
 
 }]);
