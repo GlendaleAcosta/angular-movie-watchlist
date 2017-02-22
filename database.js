d907b0afb901dd8807f56f0f2d251c1ecbb76d0c
Glendale Acosta <gglendale17@gmail.com>
2017-02-22 01:28:01 -0800

    fixed database.

--- database.js
+++ database.js
@@ -1,20 +1,20 @@
 var pgp = require('pg-promise')();
 
 // Database Config
-var connection = {
-    host: 'localhost',
-    port: 5433,
-    database: 'movie_watchlist',
-    user: 'postgres',
-    password: 'rootpw'
-} 
 // var connection = {
-//     host: 'ec2-23-23-225-116.compute-1.amazonaws.com',
-//     port: 5432,
-//     database: 'd9n4svelcktveq',
-//     user: 'gsrlqmwyfrxjup',
-//     password: 'b7349fe4b623f5035f4566cff63340c256aa8cd30923ca0fd689df95c4640855'
-// }
+//     host: 'localhost',
+//     port: 5433,
+//     database: 'movie_watchlist',
+//     user: 'postgres',
+//     password: 'rootpw'
+// } 
+var connection = {
+    host: 'ec2-23-23-225-116.compute-1.amazonaws.com',
+    port: 5432,
+    database: 'd9n4svelcktveq',
+    user: 'gsrlqmwyfrxjup',
+    password: 'b7349fe4b623f5035f4566cff63340c256aa8cd30923ca0fd689df95c4640855'
+}
 // Database Connection
 var db = pgp(connection);
 
